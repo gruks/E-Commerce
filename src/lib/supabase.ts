5cb188809fc1c603e1a3016979539d21a66454d4
gruks <notastheticallyayush@gmail.com>
2026-02-07 12:17:50 +0530

    fix supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,5 +1,5 @@
 import { createClient } from '@supabase/supabase-js';
-import { Database } from '@/srctypes/database';
+import { Database } from '@/src/types/database';
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
